f24445c57c3dc7e5c5f5bc020900624174911e43
jjokiba <junijademaia@gmail.com>
2022-11-02 19:52:47 -0300

    Bug - Quickfix with temporary issuehandler

--- components/textEditor/textEditor.tsx
+++ components/textEditor/textEditor.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { ComponentClass, ComponentType, useState } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
+import SpinLoader from "../spinLoader/spinLoader";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
@@ -8,23 +9,41 @@ interface TextEditorProps {
 	id: string;
 	className: string;
 	value: string;
-	onChange: Function;
+	onChange: (val: any) => void;
+}
+
+async function RequestQuill(setQuillComponent: Function, setAwait: Function) {
+	setQuillComponent(dynamic((async () => await import("react-quill")), {ssr: false}));
+
+	await new Promise((resolve) => {setTimeout(() => {resolve("");}, 1000);});;
+	setAwait(true);
 }
 
 const TextEditor = ({ id, className, value, onChange }: TextEditorProps) => {
-	return (
-		<>
-			<ReactQuill
-				id={id}
-				className={className}
-				theme="snow"
-				value={value}
-				onChange={(e: any) => {
-					onChange(e);
-				}}
-			></ReactQuill>
-		</>
-	);
+	const [QuillComponent, setQuillComponent] = useState<ComponentType>();
+	const [awaitQuill, setAwait] = useState<boolean>(false);
+	if (!awaitQuill && QuillComponent == undefined) {
+		RequestQuill(setQuillComponent, setAwait);
+	}
+	if (!awaitQuill) {
+		return (
+			<>
+				<SpinLoader/>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<QuillComponent
+					id={id}
+					className={className}
+					theme="snow"
+					value={value}
+					onChange={onChange}
+				></QuillComponent>
+			</>
+		);
+	}
 };
 
 export default TextEditor;
